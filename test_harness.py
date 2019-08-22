934f5cc284bae9bdf718c9bc49ad3a0216709fb4
Joe J Collins <joejcollins@gmail.com>
2019-08-22 21:20:43 +0100

    Running on WSL

--- test_harness.py
+++ test_harness.py
@@ -2,7 +2,9 @@
 # import the celery tasks
 from tasks import *
 
-# Using `.s().apply()` will cause the task to execute immediately without Celery running
+# Using `.s().apply()` will cause the task to execute immediately without a Celery
+# worker needing to be running.  However Redis does need to be running 
+# (`sudo service redis-server restart`).
 RESULT = slowly_reverse_string.s(string="qwerty").apply()
 
 # Since the task is run synchronously this won't happened u
