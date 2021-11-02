28c709c0b8fd91a62a5268ccc1c11a88055cadd2
Joe J Collins <joejcollins@gmail.com>
2021-11-02 17:28:12 +0000

    Group then task

--- shit.py
+++ shit.py
@@ -1,12 +1,15 @@
 """ Scratch pad. """
 from task_queue.text import slowly_reverse_string
-slowly_reverse_string.delay("qwerty")
-slowly_reverse_string.delay("qwerty")
-slowly_reverse_string.delay("qwerty")
-slowly_reverse_string.delay("qwerty")
-slowly_reverse_string.delay("qwerty")
-slowly_reverse_string.delay("qwerty")
-slowly_reverse_string.delay("qwerty")
-slowly_reverse_string.delay("qwerty")
-slowly_reverse_string.delay("qwerty")
 
+import celery
+
+tasks = []
+tasks.append(slowly_reverse_string.s("qwerty"))
+tasks.append(slowly_reverse_string.s("asdfgh"))
+tasks.append(slowly_reverse_string.s("zxcvbn"))
+
+# The chord passes the result of the group so use `si`
+# (an immutable signature) so the result is ignored.
+final_task = slowly_reverse_string.si("eat my shorts")
+
+chain_of_tasks = celery.chord(tasks, final_task)()
