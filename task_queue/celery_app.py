87277de344bd19d1a07a059eefa737673b307e54
Joe J Collins <joejcollins@gmail.com>
2021-11-03 16:18:01 +0000

    Separate queues

--- task_queue/celery_app.py
+++ task_queue/celery_app.py
@@ -9,5 +9,10 @@ queue.conf.update(
     result_backend="redis://localhost:6379/1",
     task_serializer="json",
     accept_content=["json", "pickle"],
-    imports=('task_queue.text', 'task_queue.numbers')
-    )
+    imports=('task_queue.text', 'task_queue.numbers'),
+    task_create_missing_queues=True,
+    task_routes={
+        "text.*": {"queue": "text_queue"},
+        "number.*": {"queue": "number_queue"},
+    },
+)
