0946e20ccb21467b3798c8cfa697875484901f61
Joe J Collins <joejcollins@gmail.com>
2021-11-03 14:09:55 +0000

    Increase delay

--- shit.py
+++ shit.py
@@ -1,15 +1,27 @@
 """ Scratch pad. """
 from task_queue.text import slowly_reverse_string
+from task_queue.numbers import slowly_add_two_numbers
 
 import celery
 
-tasks = []
-tasks.append(slowly_reverse_string.s("qwerty"))
-tasks.append(slowly_reverse_string.s("asdfgh"))
-tasks.append(slowly_reverse_string.s("zxcvbn"))
+text_tasks = []
+text_tasks.append(slowly_reverse_string.s("qwerty"))
+text_tasks.append(slowly_reverse_string.s("asdfgh"))
+text_tasks.append(slowly_reverse_string.s("zxcvbn"))
 
 # The chord passes the result of the group so use `si`
 # (an immutable signature) so the result is ignored.
-final_task = slowly_reverse_string.si("eat my shorts")
+final_text_task = slowly_reverse_string.si("eat my shorts")
 
-chain_of_tasks = celery.chord(tasks, final_task)()
+chain_of_text_tasks = celery.chord(text_tasks, final_text_task)()
+
+number_tasks = []
+number_tasks.append(slowly_add_two_numbers.s(1, 2))
+number_tasks.append(slowly_add_two_numbers.s(3, 4))
+number_tasks.append(slowly_add_two_numbers.s(5, 6))
+
+final_number_task = slowly_add_two_numbers.si(7, 8)
+
+chain_of_number_tasks = celery.chord(number_tasks, final_number_task)
+
+print("Done")
--- task_queue/numbers.py
+++ task_queue/numbers.py
@@ -6,8 +6,8 @@ import info_module.numbers as numbers
 
 @queue.task(bind=True, name='number.slowly_add_two_numbers')
 def slowly_add_two_numbers(self, first_number, second_number):
-    """ Add the numbers but take 5 seconds to do it. """
-    counter = 5
+    """ Add the numbers but take 10 seconds to do it. """
+    counter = 10
     for i in range(0, counter):
         self.update_state(state='PROGRESS', meta={'done': i, 'total': counter})
         time.sleep(1)
