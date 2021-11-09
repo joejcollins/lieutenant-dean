18583a7184a56f6985880ba2ba362dcd9f6aec9d
Joe J Collins <joejcollins@gmail.com>
2021-11-09 17:31:38 +0000

    Alternating

--- example_chain.py
+++ example_chain.py
@@ -6,6 +6,8 @@ import celery
 
 task1 = slowly_reverse_string.s("qwerty")
 task2 = slowly_reverse_string.si("asdfgh")
-task3 = slowly_reverse_string.si("zxcvbn")
+task3 = slowly_add_two_numbers.si(1, 2)
+task4 = slowly_add_two_numbers.si(3, 4)
 
-res = celery.chain(task1, task2, task3)()
+res = celery.chain(task1, task3, task2, task4)()
+print("test")
