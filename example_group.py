c2e8859a41ca74391ebfbaea193999d00dc3ed29
Joe J Collins <joejcollins@gmail.com>
2021-11-10 11:00:36 +0000

    More complicated example

---
+++ example_group.py
@@ -0,0 +1,13 @@
+""" Scratch pad. """
+from task_queue.text import slowly_reverse_string
+from task_queue.numbers import slowly_add_two_numbers
+
+import celery
+
+task1 = slowly_reverse_string.s("qwerty")
+task2 = slowly_reverse_string.s("asdfgh")
+task3 = slowly_add_two_numbers.s(1, 2)
+task4 = slowly_add_two_numbers.s(3, 4)
+
+result = celery.group(task1, task2, task3, task4)()
+print("test")
---
+++ example_multi.py
@@ -0,0 +1,51 @@
+"""
+Deleting an environment
+=======================
+
+Delete the networking
+---------------------
+
+First:
+
+* p-destroy-vlan.yml
+* p-ensure-public-ip.yml
+
+Then:
+
+* delete environment network
+
+Delete the environment itself
+-----------------------------
+
+First:
+
+* e-destroy-vms.yml (delete the VMs in Vcenter)
+* e-destroy-node-cache.yml (this is the edge cache which doesn't work).
+* e-destroy-dns.yml
+* e-destroy-monitoring.yml
+
+Then:
+
+* delete the configuration files
+
+"""
+from task_queue.text import slowly_reverse_string
+import celery
+
+delete_network_prep = []
+delete_network_prep.append(slowly_reverse_string.s("lmy.nalv-yortsed-p"))
+delete_network_prep.append(slowly_reverse_string.s("lmy.pi-cilbup-erusne-p"))
+# The final task is immutable because we don't need the result of the previous tasks.
+delete_network_final = slowly_reverse_string.si("krowten tnemnorivne eteled")
+delete_network = celery.chord(delete_network_prep, delete_network_final)
+delete_network_result = delete_network()
+
+delete_environment_prep = []
+delete_environment_prep.append(slowly_reverse_string.s("lmy.smv-yortsed-e"))
+delete_environment_prep.append(slowly_reverse_string.s("lmy.ehcac-edon-yortsed-e"))
+delete_environment_prep.append(slowly_reverse_string.s("lmy.snd-yortsed-e"))
+delete_environment_prep.append(slowly_reverse_string.s("lmy.gnirotinom-yortsed-e"))
+# The final task is immutable because we don't need the result of the previous tasks.
+delete_environment_final = slowly_reverse_string.si("krowten tnemnorivne eteled")
+delete_environment = celery.chord(delete_environment_prep, delete_environment_final)
+delete_environment_result = delete_environment()
