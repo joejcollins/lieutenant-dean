a844c70afe0ea312d94dcf5ff91ff2e3ce0a72bf
Joe Collins <j.collins@zengenti.com>
2022-04-07 13:32:12 +0100

    License

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
 """ Setup as a package so that celery can access the packages.  Then
 install with `requirements.txt` and `-e .`.  """
-from setuptools import setup, find_packages
-setup(name='captain-black', version='1.0', packages=find_packages())
+import setuptools
+
+setuptools.setup(
+    name="captain-black",
+    version="1.0",
+    author="Joe Collins",
+    author_email="joejcollins@gmail.com",
+    description="Demonstrator",
+    license="MIT License",
+    url="https://github.com/joejcollins/captain-black",
+    packages=setuptools.find_packages(),
+    package_dir={"": "./"},
+    classifiers=["Development Status :: 3 - Alpha", "License :: Other/Proprietary License"],
+    },
+)
