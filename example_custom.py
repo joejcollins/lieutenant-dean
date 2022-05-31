0c4f44c35909fa79437b997b2fa6e8821d6e1e50
Mags Cousins <mags@bagbatch.co.uk>
2022-05-31 11:39:42 +0000

    Custom demo

---
+++ example_custom.py
@@ -0,0 +1,40 @@
+import celery_apps.redis_broker as broker
+from kombu import Exchange, Queue, Consumer
+from celery import bootsteps
+
+my_queue = Queue("custom", Exchange("custom"), "routing_key")
+
+body = {
+    "Body": {"Type": "environment", "Alias": "joes-test", "Properties": {}},
+    "Topic": "joes-test.environment.delete",
+    "CallContext": {},
+}
+
+
+class MyConsumerStep(bootsteps.ConsumerStep):
+    def get_consumers(self, channel):
+        return [Consumer(channel, queues=[my_queue], callbacks=[self.handle_message], accept=["json"])]
+
+    def handle_message(self, body, message):
+        print("Received message: {0!r}".format(body))
+        message.ack()
+
+
+broker.redis_queues.steps["consumer"].add(MyConsumerStep)
+
+
+def send_me_a_message(producer=None):
+    """test custom message"""
+    with broker.redis_queues.producer_or_acquire(producer) as producer:
+        producer.publish(
+            body=body,
+            serializer="json",
+            exchange=my_queue.exchange,
+            routing_key="routing_key",
+            declare=[my_queue],
+            retry=True,
+        )
+
+
+if __name__ == "__main__":
+    send_me_a_message()
