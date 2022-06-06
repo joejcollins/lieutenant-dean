180f385bb33a44437ecdb49c9a6c974dd4c868f5
Mags Cousins <mags@bagbatch.co.uk>
2022-06-06 17:28:04 +0000

    Custom examples

--- example_custom.py
+++ example_custom.py
@@ -9,19 +9,19 @@ import task_queuing.celery_app as app
 
 def send_to_shit(producer=None):
     """Send a custom message as per Scott's instructions."""
-    my_queue = Queue('custom_queue', Exchange('custom_queue'), 'custom_queue')
+    my_queue = Queue("custom_queue", Exchange("custom_queue"), "custom_queue")
     eta = str(datetime.now())
     id = celery.uuid()
     # headers = {"task": "task_queuing.tasks.custom.shit", "id": id}
     # body = {"text": "Secret text to encode", "Time": current_time}
 
     message = {
-        'task': 'task_queuing.tasks.custom.shit',
-        'id': id,
-        'args': [id],
+        "task": "task_queuing.tasks.custom.shit",
+        "id": id,
+        "args": [id],
         "kwargs": {},
         "retries": 0,
-        "eta": eta
+        "eta": eta,
     }
 
     with app.queue_broker.producer_or_acquire(producer) as producer:
@@ -36,22 +36,15 @@ def send_to_shit(producer=None):
         return id
 
 
-def send_to_reverse_text(producer=None):
+def send_to_custom_consumer(producer=None):
     """Send a custom message as per Scott's instructions."""
     # For Celery the queue and routing key should match.
-    my_queue = Queue(name='text_queue', routing_key='text_queue')
-    eta = str(datetime.now())
-    id = celery.uuid()
-    # headers = {"task": "task_queuing.tasks.custom.shit", "id": id}
-    # body = {"text": "Secret text to encode", "Time": current_time}
+    my_queue = Queue(name="text_queue", routing_key="text_queue")
 
     message = {
-        'task': 'task_queuing.tasks.text.slowly_reverse_string',
-        'id': id,
-        'args': [id],
-        "kwargs": {},
-        "retries": 0,
-        "eta": eta
+        "Body": {"Type": "environment", "Alias": "{{alias}}", "Properties": {}},
+        "Topic": "{{alias}}.environment.{{action}}",
+        "CallContext": {},
     }
 
     with app.queue_broker.producer_or_acquire(producer) as producer:
@@ -60,6 +53,33 @@ def send_to_reverse_text(producer=None):
             serializer="json",
             exchange=my_queue.exchange,
             routing_key="custom_queue",
+            declare=[my_queue],
+            retry=True,
+        )
+        return id
+
+
+def send_to_text_reverse(producer=None):
+    my_queue = Queue("text_queue", Exchange("text_queue"), "text_queue")
+    eta = str(datetime.now())
+    id = celery.uuid()
+    # headers = {"task": "task_queuing.tasks.custom.shit", "id": id}
+    # body = {"text": "Secret text to encode", "Time": current_time}
+
+    message = {
+        "task": "task_queuing.tasks.text.slowly_reverse_string",
+        "id": id,
+        "args": ["reverse me"],
+        "kwargs": {},
+        "retries": 0
+    }
+
+    with app.queue_broker.producer_or_acquire(producer) as producer:
+        producer.publish(
+            message,
+            serializer="json",
+            exchange=my_queue.exchange,
+            routing_key="text_queue",
             declare=[my_queue],
             retry=True,
         )
@@ -67,6 +87,5 @@ def send_to_reverse_text(producer=None):
 
 
 if __name__ == "__main__":
-    id = send_to_shit()
+    id = send_to_text_reverse()
     print(f"task {id} on queue")
-    
