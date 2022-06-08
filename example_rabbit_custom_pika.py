ebab7da6c3359c39eb360d71535d5dd40e7b0048
kjchesh <katrina.cheshire94@gmail.com>
2022-06-08 08:52:23 +0000

    send custom message

--- example_rabbit_custom_pika.py
+++ example_rabbit_custom_pika.py
@@ -1,23 +1,22 @@
 """ Scratch pad. """
 import pika
+import json
 
 connection = pika.BlockingConnection(pika.ConnectionParameters("localhost", 5672))
 channel = connection.channel()
 
 channel.queue_declare(queue="text_queue", durable=True)
 
-# message = {
-#     "Body": {"Type": "environment", "Alias": "{{alias}}", "Properties": {}},
-#     "Topic": "{{alias}}.environment.{{action}}",
-#     "CallContext": {},
-# }
-
-message = "hello world"
+message = {
+    "Body": {"Type": "environment", "Alias": "{{alias}}", "Properties": {}},
+    "Topic": "{{alias}}.environment.{{action}}",
+    "CallContext": {},
+}
 
 channel.basic_publish(
     exchange="",
     routing_key="text_queue",
-    body=message,
+    body=json.dumps(message),
     properties=pika.BasicProperties(delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE),
 )
 
