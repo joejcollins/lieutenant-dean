021584e8f92c235784f03b27b27d3b4be14e2f96
Joe J Collins <j.collins@zengenti.com>
2024-01-15 21:34:02 +0000

    Redundant inverted commas

--- celery_rabbit/consumers.py
+++ celery_rabbit/consumers.py
@@ -1,4 +1,4 @@
-"""Custom consumers for celery_rabbit."""""
+"""Custom consumers for celery_rabbit."""
 from celery import bootsteps
 from celery_queue_rabbit.queues import my_queues
 from kombu import Consumer
